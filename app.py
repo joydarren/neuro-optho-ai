1426fbdde1d3295d6dd7e09c26bd2649f7da692a
joydarren <joydarren06@gmail.com>
2026-03-21 17:32:35 +0530

    fix port binding

--- app.py
+++ app.py
@@ -229,6 +229,8 @@ def predict():
         "risk_level":level
     })
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
